f67b2bcb681a296ca51df1e711268dd22e927d8b
zb2313 <57031778+zb2313@users.noreply.github.com>
2022-06-15 16:36:40 +0800

    随机食物

--- Vuforia_demo/Assets/Scripts/SetFood.cs
+++ Vuforia_demo/Assets/Scripts/SetFood.cs
@@ -9,16 +9,34 @@ public class SetFood : MonoBehaviour
     private GameObject cube;
     public Transform parent;
     public Transform createCube;
+    public string createFoodName;
+    public int createFoodNumber;
+    public class FoodName
+    {
+        public string foodname;
+        public int foodnumber;
+        public FoodName(string a,int b)
+        {
+            foodname = a;
+            foodnumber = b;
+        }
+    };
+    FoodName[] foodNames = {
+            new FoodName("Banana",1), new FoodName("Cheese",2) , new FoodName("Cherry",3) , new FoodName("Hamburger",4) ,
+        new FoodName("Hotdog",5),new FoodName("Olive",6),new FoodName("Watermelon",7) };
     void Start()
     {
-        cube = Resources.Load("Prefabs/Cube") as GameObject;
+        
     }
     public void OnSetFoodClick()
     {
         //GameObject ob = new GameObject("food");
-        
-
+        int tnumber = Random.Range(0, 7);
+        createFoodName = foodNames[tnumber].foodname;
+        createFoodNumber = foodNames[tnumber].foodnumber;
+        cube = Resources.Load("Mg3D_Food/" + createFoodName) as GameObject;
         GameObject obj = Instantiate(cube);
+        obj.transform.localScale = new Vector3(1, 1, 1);
         obj.name = "food";
         Vector3 loc = new Vector3(Random.Range(-1.5f, 1.5f), 0, Random.Range(-1.5f, 1.5f));
         obj.transform.position = createCube.transform.TransformPoint(loc);
